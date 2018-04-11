0372ad7efcbe50a380b0f745fd380092d8a55a3b
ojhernandezcepeda <36193487+ojhernandezcepeda@users.noreply.github.com>
2018-04-10 20:16:23 -0500

    Update arb0lBin.py

--- ARB0L_Bin/arb0lBin.py
+++ ARB0L_Bin/arb0lBin.py
@@ -1,1 +1,37 @@
+class ArbolBin:
+    def __init__(self, valor,izq=None,der=None):
+        self.valor=valor
+        self.izquierda=izq
+        self.derecha=der
 
+def buscar(arbol,valor):
+    if arbol==None:
+        return False
+    if arbol.valor==valor:
+        return True
+    if valor<arbol.valor:
+        return buscar(arbol.izquierda,valor)
+    return buscar(arbol.derecha,valor)
+
+def inOrden(arbol):
+  if arbol != None:
+      inOrden(arbol.izquierda)
+      print(arbol.valor)
+      inOrden(arbol.derecha)
+      
+def insertar (arbol,valor):
+    if arbol==None:
+        return ArbolBin(valor)
+    if valor<arbol.valor:
+        return ArbolBin(arbol.valor,insertar(arbol.izquierda,valor),arbol.derecha)
+    return ArbolBin(arbol.valor,arbol.izquierda,insertar(arbol.derecha,valor))
+      
+def listarEnArbol(arbol, lista):
+    if lista==[]:
+        return arbol
+    else:
+        return listarEnArbol(insertar(arbol,lista[0]),lista[1:])
+    
+   
+
+inOrden(listarEnArbol((ArbolBin(15,ArbolBin(10,ArbolBin(0)),ArbolBin(55, ArbolBin(25)))),[5,50,20,30,35,45,40]))
